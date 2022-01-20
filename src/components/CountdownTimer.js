d563bb6918b22961126dec5db997afe4f708b9da
Jee Ryann Genio <jeegenio@gmail.com>
2022-01-20 14:16:27 +0800

    modified countdowntimer

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from "react";
 function CountdownTimer() {
   const calculateTimeLeft = () => {
     let timeLeft = {};
-    let year = +new Date().getFullYear();
-    const difference = +new Date(`10/01/${year}`) - +new Date();
+    const difference = +new Date("01/23/2022") - +new Date();
 
     if (difference > 0) {
       timeLeft = {
@@ -17,33 +16,44 @@ function CountdownTimer() {
     return timeLeft;
   };
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  console.log(timeLeft);
+  // console.log(timeLeft);
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
-    return () => clearTimeout(timer);
-  });
+    return () => clearInterval(timer);
+  }, [timeLeft]);
 
-  const timerComponents = [];
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
+  // console.log(timeLeft);
+  // const timerComponents = [];
+  // Object.keys(timeLeft).forEach((interval) => {
+  //   if (!timeLeft[interval]) {
+  //     return;
+  //   }
 
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
+  //   timerComponents.push(
+  //     <span>
+  //       {timeLeft[interval]} {interval}{" "}
+  //     </span>
+  //   );
+  // });
 
   return (
     <div>
       <div>
         <h1>SALE IS LIVE</h1>
       </div>
-      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      {/* {timerComponents.length ? timerComponents : <span>Time's up!</span>} */}
+      {timeLeft.seconds === 0 && timeLeft.minutes === 0 ? (
+        <h1> Times up!!</h1>
+      ) : (
+        <h1 style={{ color: "blue" }}>
+          {timeLeft.days} {timeLeft.hours} {timeLeft.minutes} {timeLeft.seconds}
+        </h1>
+      )}
+      <div>
+        <h5>days</h5>
+      </div>
     </div>
   );
 }
